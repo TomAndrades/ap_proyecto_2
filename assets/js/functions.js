252d7141282cd4ae450e90aec647378c1f3c1e1c
Tomas <andradestom@gmail.com>
2023-03-10 22:28:20 -0300

    search funcionando en index.html

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,59 +1,59 @@
 
 
 function getConditionalEvents(data, time = 0) {
-    //Toma una lista de data y retorna los eventos de un conjunto de datos,   
-    let events = []
-    switch (time) {
-        case -1:
-            //before currentDate events
-            for (let i = 0; i < data.events.length; i++) {
-                if (data.currentDate > data.events[i].date)
-                    events.push(data.events[i])
-            }
-            break;
-        case 0:
-            //all events
-            for (let i = 0; i < data.events.length; i++) {
-                events.push(data.events[i]);
-            }
-            break;
-        case 1:
-            //after currentDate events
-            for (let i = 0; i < data.events.length; i++) {
-                if (data.currentDate < data.events[i].date)
-                    events.push(data.events[i])
-            }
-            break;
-        default:
-            console.error('Debes agregar el segundo parametro correctamente');
-            break;
-    }
+  //Toma una lista de data y retorna los eventos de un conjunto de datos,   
+  let events = []
+  switch (time) {
+    case -1:
+      //before currentDate events
+      for (let i = 0; i < data.events.length; i++) {
+        if (data.currentDate > data.events[i].date)
+          events.push(data.events[i])
+      }
+      break;
+    case 0:
+      //all events
+      for (let i = 0; i < data.events.length; i++) {
+        events.push(data.events[i]);
+      }
+      break;
+    case 1:
+      //after currentDate events
+      for (let i = 0; i < data.events.length; i++) {
+        if (data.currentDate < data.events[i].date)
+          events.push(data.events[i])
+      }
+      break;
+    default:
+      console.error('Debes agregar el segundo parametro correctamente');
+      break;
+  }
 
-    return events
+  return events
 }
 
 function htmlAdder(elementId, elementToInsert) {
-    //Toma un padre del documento por el ID y un string a insertar y lo inserta en el html
-    let element = document.getElementById(elementId);
-    element.innerHTML = elementToInsert;
+  //Toma un padre del documento por el ID y un string a insertar y lo inserta en el html
+  let element = document.getElementById(elementId);
+  element.innerHTML = elementToInsert;
 }
 
 function getCategorys(events) {
-    // Toma un array de eventos y devuelve un array de categorias unicas
-    let categorys = []
+  // Toma un array de eventos y devuelve un array de categorias unicas
+  let categorys = []
 
-    for (const event of events) {
-        if (categorys.indexOf(event.category) == -1) {
-            categorys.push(event.category);
-        }
+  for (const event of events) {
+    if (categorys.indexOf(event.category) == -1) {
+      categorys.push(event.category);
     }
-    return categorys
+  }
+  return categorys
 }
 
 function categoryInserter(categorys) {
-    let templateCheckbox = ""
-    for (const category of categorys) {
-        templateCheckbox += `<label class="form-check-label">
+  let templateCheckbox = ""
+  for (const category of categorys) {
+    templateCheckbox += `<label class="form-check-label">
       <input
         class="form-check-input"
         type="checkbox"
@@ -62,25 +62,26 @@ function categoryInserter(categorys) {
       />
       ${category}
       </label>`
-    }
-    templateCheckbox += `<form class="form d-flex flex-nowrap ms-auto mw-100">
+  }
+  templateCheckbox += `<form class="form d-flex flex-nowrap ms-auto mw-100" >
                     <input
                       class="form-control"
                       type="search"
                       placeholder="Search"
                       aria-label="Search"
+                      id="search"
                     />
-                    <button class="btn btn-fucsia btn-outline-dark py-2" type="submit">
+                    <button class="btn btn-fucsia btn-outline-dark py-2" type="submit" id="searchBtn">
                       <img src="./assets/img/search.svg" alt="Search icon" />
                     </button>
                   </form>`
-    htmlAdder("checkboxGroup", templateCheckbox)
+  htmlAdder("checkboxGroup", templateCheckbox)
 }
 
 function cardInserter(events) {
-    let templateCards = ""
-    for (const event of events) {
-        templateCards += `<div class="col">
+  let templateCards = ""
+  for (const event of events) {
+    templateCards += `<div class="col">
         <div class="card text-bg-dark">
           <img
             src="${event.image}"
@@ -101,7 +102,7 @@ function cardInserter(events) {
           </div>
         </div>
       </div>`;
-    }
+  }
 
-    htmlAdder("eventsBody", templateCards)
+  htmlAdder("eventsBody", templateCards)
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
-const events = getConditionalEvents(data)
-const categorys = getCategorys(events)
+const events = getConditionalEvents(data);
+const categorys = getCategorys(events);
+
 
 cardInserter(events)
 categoryInserter(categorys)
+function searchEvent(eventos, value) {
+  // for (let evento of eventos) {
+  //   if (evento.category == value) {
+  //     eventos_filtrados.push(evento)
+  //   }
+  // }
+  let eventos_filtrados = eventos.filter((evento) => evento.name == value)
+  return eventos_filtrados
+}
 
-let checkboxs = document.querySelector('input[type=checkbox]', 'checkboxGroup')
-console.log(checkboxs);
+function mensaje() {
+  console.log('click');
+}
+
+// const searchBtn = document.getElementById("searchBtn")
+
+// searchBtn.addEventListener('click', mensaje())
+
+let search = document.querySelector('#search')
+let searchBtn = document.querySelector('#searchBtn')
+
+searchBtn.addEventListener('click', (evento) => {
+  let value = search.value.toLowerCase();
+  let eventosFiltrados = events.filter((elemento) => (elemento.name.toLowerCase()).match(value))
+  cardInserter(eventosFiltrados)
+  evento.preventDefault();
+}
+);
