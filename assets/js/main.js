c60e375722014dde4d5640cbb49be38a63949e54
Tomas <andradestom@gmail.com>
2023-03-12 13:28:38 -0300

    filtros cruzados working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 const events = getConditionalEvents(data);
 const categorys = getCategorys(events);
+let showingElements = events;
 
-
-cardInserter(events)
+cardInserter(showingElements)
 categoryInserter(categorys)
 
 function searchByName(eventos, value) {
-  let eventosFiltrados = events.filter((elemento) => (elemento.name.toLowerCase()).match(value.toLowerCase()))
+  let eventosFiltrados = eventos.filter((elemento) => (elemento.name.toLowerCase()).match(value.toLowerCase()))
   return eventosFiltrados
 }
 
@@ -19,37 +19,34 @@ function mensaje() {
   console.log('click');
 }
 
-// const searchBtn = document.getElementById("searchBtn")
-
-// searchBtn.addEventListener('click', mensaje())
 
 let search = document.querySelector('#search')
-let searchBtn = document.querySelector('#searchBtn')
 
+
+const searchBtn = document.querySelector('#searchBtn')
 searchBtn.addEventListener('click', (evento) => {
   let value = search.value;
-  cardInserter(searchByName(events, value))
+  cardInserter(searchByName(showingElements, value))
   evento.preventDefault();
 }
 );
 
 
-let checkboxGroup = document.querySelector('#checkboxGroup')
 
+const checkboxGroup = document.querySelector('#checkboxGroup')
 checkboxGroup.addEventListener('change', () => {
   let checkboxes = document.querySelectorAll('input[type="checkbox"]');
   let checked = Array.from(checkboxes).filter((checkbox) => checkbox.checked)
   let checkedValues = checked.map(element => element.value.toLowerCase())
 
-  if (checked.some((element) => element.checked)){
+  if (checked.some((element) => element.checked)) {
     let eventosFiltrados = []
     checkedValues.forEach(element => {
-      eventosFiltrados.push(searchByCategory(events, element))
+      eventosFiltrados.push(searchByCategory(showingElements, element));
+      showingElements = eventosFiltrados.flat();
     });
-    cardInserter(eventosFiltrados.flat());
-  }else{
-    cardInserter(events);
+  } else {
+    showingElements = events;
   }
-  
-
+  cardInserter(showingElements);
 });
