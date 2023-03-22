209257ddecc9aaaa968bc005a5f34d1351db47e2
tomasandradesASUS <andradestom@gmail.com>
2023-03-22 00:11:49 -0300

    agregado un poco de color en details

--- assets/js/details.js
+++ assets/js/details.js
@@ -7,19 +7,19 @@ const main = document.querySelector("main")
 main.innerHTML = `
 <div class="d-flex flex-column flex-xl-row w-75 text-bg-dark py-3 px-3">
         <div
-          class="d-flex justify-content-center align-items-center bg-secondary"
+          class="d-flex justify-content-center align-items-center bg-dark"
         >
           <img src="${evento.image}" alt="${evento.name}" class="w-100" />
         </div>
         <div
           class="text-center justify-content-center d-flex flex-nowrap flex-column bg-secondary"
         >
-          <h2>${evento.name}</h2>
+          <h2 class="fw-bold shadow bg-dark">${evento.name}</h2>
           <p class="fs-3">${evento.description}</p>
-          <p class="fs-3">Capacity: ${evento.capacity}</p>
-          <p class="fs-3">Place: ${evento.place}</p>
-          <p class="fs-3">Price: $${evento.price}</p>
-          <p class="fs-3">Date: ${evento.date}</p>
+          <p class="fs-3 bg-fucsia "><span class="fst-italic">Capacity:</span> ${evento.capacity}</p>
+          <p class="fs-3 bg-fucsia "><span class="fst-italic">Place:</span> ${evento.place}</p>
+          <p class="fs-3 bg-fucsia "><span class="fst-italic">Price:</span> $${evento.price}</p>
+          <p class="fs-3 bg-fucsia "><span class="fst-italic">Date:</span> ${evento.date}</p>
           </div>
       </div>
       `
