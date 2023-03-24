b2ee7a379cb8cd7e953d5e81df1e298179dc4be9
tomasandradesASUS <andradestom@gmail.com>
2023-03-24 09:48:12 -0300

    empezando a aplicar vue en la app

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
-const events = getConditionalEvents();
-let showingElements = events;
+
+
+  const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        message: 'Hello Vue!'
+      }
+    }
+  }).mount('#app')
+// const events = getConditionalEvents();
+// let showingElements = events;
 let search = document.querySelector('#search')
 let searchBtn = document.querySelector('#searchBtn')
 let checkboxGroup = document.querySelector('#checkboxGroup')
-
